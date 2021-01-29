cb9d7c03de636c02ea42b51aa1ce340306d98eac
James Stafford-Coyte <jstaffordcoyte@gmail.com>
2021-01-29 14:18:53 -0800

    removed unused imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,6 @@ import Navigation from './navigation';
 import Header from './header';
 import Features from './features';
 import About from './about';
-import Services from './services';
-import Gallery from './gallery';
-import Testimonials from './testimonials';
-import Team from './Team';
 import Contact from './contact';
 import $ from 'jquery';
 
