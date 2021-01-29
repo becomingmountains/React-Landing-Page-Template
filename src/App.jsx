05b3ddb28319361e4d8ff61f4a7ae2936d12d23a
James Stafford-Coyte <jstaffordcoyte@gmail.com>
2021-01-29 14:11:31 -0800

    Removed unnecessary components

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,6 @@ export class App extends Component {
         <Header data={this.state.landingPageData.Header} />
         <Features data={this.state.landingPageData.Features} />
         <About data={this.state.landingPageData.About} />
-        <Services data={this.state.landingPageData.Services} />
-        <Gallery />
-        <Testimonials data={this.state.landingPageData.Testimonials} />
-        <Team data={this.state.landingPageData.Team} />
         <Contact data={this.state.landingPageData.Contact} />
       </div>
     )
