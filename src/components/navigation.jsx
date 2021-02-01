66e37d269fa33e08fab0721ad1291edc7ee9da93
James Stafford-Coyte <jstaffordcoyte@gmail.com>
2021-02-01 13:52:20 -0800

    Update navigation.jsx

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from '../../Murmuring-01.jpg'
+import logo from '../Murmuring-01.jpg'
 
 export class Navigation extends Component {
   render() {
