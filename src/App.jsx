577b3980e3075682dfcf4996b935f822abadd44c
james <james@pop-os.localdomain>
2021-06-15 17:11:31 -0700

    added privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 import Header from './components/header';
 import Contact from './components/contact';
 import JsonData from './data/data.json';
-
+import Privacy from './components/Privacy';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 export class App extends Component {
   state = {
     landingPageData: {},
@@ -18,8 +25,11 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Header data={this.state.landingPageData.Header} />
-        <Contact data={this.state.landingPageData.Contact} />
+        <Router>
+          <Header data={this.state.landingPageData.Header} />
+          <Contact data={this.state.landingPageData.Contact} />
+          <Route path="/privacy" component={Privacy} />
+        </Router>
       </div>
     )
   }
